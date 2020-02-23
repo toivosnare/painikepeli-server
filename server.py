8cc10cbd6749e8347dd3ab57be6e5a4e2bccbd4a
Toivo Snåre <topisn@gmail.com>
2020-02-23 16:28:23 +0200

    vastaa pisteiden hakuun

--- server.py
+++ server.py
@@ -3,20 +3,23 @@
 import os
 import asyncio
 import websockets
+import json
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+players = {}
+counter = 0
 
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+async def response(websocket, path):
+    msg = json.loads(await websocket.recv())
+    name = msg["name"]
+    if name not in players:
+        players[name] = 20
+    websocket.send(str(players[name]))
+    
 
 port = os.environ['PORT']
 host = "0.0.0.0"
 print("Starting server on port", port)
-start_server = websockets.serve(hello, host, port)
+start_server = websockets.serve(response, host, port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
